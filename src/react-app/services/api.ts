32e8536f6cb3760dd18398a18584e814ccc928c0
THADEM RAKESH <thademrakesh158@gmail.com>
2026-03-22 00:56:20 +0530

    Update api.ts

--- src/react-app/services/api.ts
+++ src/react-app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://instigative-overvaluably-rozella.ngrok-free.dev';
 
 const api = axios.create({
   baseURL: API_URL,
